ed037760c2898a9e77e06bd86f6addac8076e076
Ángel Iván Martínez <mgangelivan@gmail.com>
2023-01-30 12:52:36 -0600

    cambios footer movil

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="footer grid col-4 peq-col-1">
+<footer class="footer grid col-4 ">
         <div class="footer-description-column flex f-column">
             <img class="footer-logo" src="<?php echo(IMG. 'Imagotipo-FREQUENTUM-1.svg')?>" alt="logo">
             <div class="description-text-container">
@@ -9,7 +9,7 @@
             </div>
         </div>
 
-        <div class="footer-contacto-column flex f-column">
+        <div class="footer-contacto-column flex f-column peq-span-2">
             <div class="footer-phone-section flex">
                 <img src="<?php echo(ICON. 'telefono-morado-icon.svg')?>" alt="footer-phone-icon" class="footer-phone-icon">
                 <div class="contact-footer-number-container flex f-column">
@@ -17,6 +17,7 @@
                     <h4 class= "footer-number">Cel. 55 2486 9444</h4>
                 </div>
             </div>
+
             <div class="footer-mail-section flex">
                 <img src="<?php echo(ICON. 'correo-morado-icon.svg')?>" alt="footer-mail-icon" class="footer-mail-icon">
                 <div class="footer-mails-container flex f-column">
@@ -24,13 +25,14 @@
                     <h4 class="footer-mails-correo">notificaciones@frequentum.mx</h4>
                 </div>
             </div>
+
             <div class="footer-location-seccion flex">
                 <img src="<?php echo(ICON. 'ubicacion-morado-icon.svg')?>" alt="footer-location-icon" class="footer-location-icon">
                 <p>Ángel Urraza 807, Primer Piso Col. del Valle Centro, Benito Juárez, 03100 Ciudad de México, CDMX.</p>
             </div>
         </div>
 
-        <div class="footer-menu-column flex f-column">
+        <!-- <div class="footer-menu-column flex f-column">
             <h4 class="footer-menu-title">FREQUENTUM</h4>
             <div class="menu-pie-menu-container">
                 <ul class="menu-pie-menu flex f-column">
@@ -45,7 +47,7 @@
                     </li>
                 </ul>
             </div>
-        </div>
+        </div> -->
 
         <div class="footer-mascota-container flex f-column">
             <img src="<?php echo(IMG.'MascotaCentada.webp')?>" alt="footer-mascota" class="footer-mascota">
@@ -54,17 +56,21 @@
                 <a href="#">
                     <img src="<?php echo(ICON. 'facebookBlanco.svg')?>" alt="footer-mascota-icon" class="flex face-icon footer-mascota-icon">
                 </a>
+
                 <a href="#">
                     <img src="<?php echo(ICON. 'insta-icon.svg')?>" alt="insta-icon" class="footer-mascota-icon">
                 </a>
+
                 <a href="#">
                     <img src="<?php echo(ICON. 'linkedin-icon-blanco.svg')?>" alt="footer-mascota-icon" class="footer-mascota-icon">
                 </a>
+
                 <a href="#">
                     <img src="<?php echo(ICON. 'whats-icon-blanco.svg')?>" alt="footer-mascota-icon" class="footer-mascota-icon">
                 </a>
+
                 <a href="#">
-                <img src="<?php echo(ICON. 'twiter-icon-blanco.svg')?>" alt="footer-mascota-icon" class="footer-mascota-icon">
+                    <img src="<?php echo(ICON. 'twiter-icon-blanco.svg')?>" alt="footer-mascota-icon" class="footer-mascota-icon">
                 </a>
             </div>
         </div>
