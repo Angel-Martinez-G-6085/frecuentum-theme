46262a80d27f4e3786305967e2575d4357974b6b
LuiEML <guero560mlb@gmail.com>
2023-02-23 15:23:11 -0600

    nuevos cambios 23-02-2023

--- js/modal/modal.js
+++ js/modal/modal.js
@@ -4,3 +4,72 @@ const BotonModal = document.querySelector(".formulario-link");
 BotonModal.addEventListener("click", () => {
     MODAL.classList.add('modal-show');
 })
+
+// modal 2
+const Modal2 = document.getElementById('modal-2');
+const BotonModal2 = document.getElementById("mod2");
+
+BotonModal2.addEventListener("click", () => {
+    Modal2.classList.add('modal-show');
+    MODAL.classList.remove('modal-show');
+})
+
+// modal NO
+const ModalNo = document.getElementById('modalNo');
+const BotonModalNo = document.getElementById("modNo");
+
+BotonModalNo.addEventListener("click", () => {
+    ModalNo.classList.add('modal-show');
+    Modal2.classList.remove('modal-show');
+})
+
+// modal 3
+const Modal3 = document.getElementById('modal-3');
+const BotonModal3 = document.getElementById("mod3");
+
+BotonModal3.addEventListener("click", () => {
+    Modal3.classList.add('modal-show');
+    Modal2.classList.remove('modal-show');
+})
+
+// modal 4
+const Modal4 = document.getElementById('modal-4');
+const BotonModal4 = document.querySelector("#mod4");
+
+
+BotonModal4.addEventListener("click", () => {
+    Modal4.classList.add('modal-show');
+    Modal3.classList.remove('modal-show');
+})
+
+// modal 5
+const Modal5 = document.getElementById('modal-5');
+const BotonModal5 = document.getElementById("mod5");
+
+BotonModal5.addEventListener("click", () => {
+    Modal5.classList.add('modal-show');
+})
+
+// modal 6
+const Modal6 = document.getElementById('modal-6');
+const BotonModal6 = document.getElementById("mod6");
+
+BotonModal6.addEventListener("click", () => {
+    Modal6.classList.add('modal-show');
+})
+
+// modal 7
+const Modal7 = document.getElementById('modal-7');
+const BotonModal7 = document.getElementById("mod7");
+
+BotonModal7.addEventListener("click", () => {
+    Modal7.classList.add('modal-show');
+})
+
+// modal 8
+const Modal8 = document.getElementById('modal-8');
+const BotonModal8 = document.getElementById("mod8");
+
+BotonModal8.addEventListener("click", () => {
+    Modal8.classList.add('modal-show');
+})
--- parts/modal/modal.php
+++ parts/modal/modal.php
@@ -1,8 +1,87 @@
 <div class="modal-container">
-    <h4 class="modal-pregunta">¿Estas interesado en  prestar servicios relacionados con...?</h4>
+    <h4 class="modal-pregunta">¡Bienvenido a FREQUENCYLEX CONSULTORIA!</h4>
+    <h6 class="modal-sub-pregunta">Estamos a unos clicks de ayudarte</h6>
+    <h6 class="modal-subtitle-pregunta">¡Vamos!</h6>
+    <div id="mod2" class="modal-links-container">
+      <img class="person" src="<?php echo(ICON. 'personaje.svg')?>" alt="opcion-image">
+    </div>
+</div>
+
+<div class="modal-container" id="modal-2">
+    <h4 class="modal-pregunta">¿Eres actualmente concesionario y/o autorizado para la prestación de servicios de telecomunicaciones y/o radiofusión?</h4>
     <div class="modal-links-container">
-        <a href="#" class="modal-link">HOLA</a>
-        <a href="#" class="modal-link">HOLA</a>
-        <a href="#" class="modal-link">HOLA</a>
+        <a id="mod3" href="#" class="modal-link">Si</a>
+        <a id="modNo" href="#" class="modal-link">No</a>
+        <a href="#" class="modal-link">Hablar con una consultora ahora</a>
+    </div>
+</div>
+
+<!-- No -->
+<div class="modal-container" id="modalNo">
+    <h4 class="modal-pregunta">¿Estás interesado en prestar servicios relacionado con...?</h4>
+    <div id="mod4" class="modal-links-container">
+        <a href="#" class="img-modal"><img  src="<?php echo(ICON. 'radio-morado.svg')?>" alt="opcion-image">Televisión</a>
+        <a href="#" class="img-modal"><img  src="<?php echo(ICON. 'radio-morado.svg')?>" alt="opcion-image">Radio</a>
+        <a href="#" class="img-modal"><img  src="<?php echo(ICON. 'radio-morado.svg')?>" alt="opcion-image">Internet</a>
+        <a href="#" class="img-modal"><img  src="<?php echo(ICON. 'radio-morado.svg')?>" alt="opcion-image">Telefonía</a>
+    </div>
+    <a id="mod-4" href="#" class="modal-link">No estoy seguro</a>
+</div>
+
+<!-- --si -->
+<div class="modal-container" id="modal-3">
+    <h4 class="modal-pregunta">¿Te gustaria recibir asesoría relacionada con algún trámite para...?</h4>
+    <div id="mod4" class="modal-links-container">
+        <a href="#" class="modal-link">INE</a>
+        <a href="#" class="modal-link">SEGOG</a>
+        <a href="#" class="modal-link">PROFECO</a>
+        <a href="#" class="modal-link">IFT</a>
+        <a href="#" class="modal-link">OTRO</a>
+    </div>
+</div>
+
+<!-- --INE, SEGOG ETC. -->
+<div class="modal-container" id="modal-4">
+    <h4 class="modal-pregunta">¡Cuéntanos!¿Con quien haremos equipo hoy?</h4>
+    <div class="modal-links-container">
+        <input class="input-link" type="text" placeholder="Nombre...">
+        <a id="mod5" href="#" class="following"></a>        
+    </div>
+</div>
+
+<!-- --Nombre - empresa -->
+<div class="modal-container" id="modal-5">
+    <h4 class="modal-pregunta">¿Cual es el nombre del proyecto/empresa en el que trabajas?</h4>
+    <div class="modal-links-container">
+        <input class="input-link" type="text" placeholder="Empresa...">
+        <a id="mod6" href="#" class="following"></a>       
+    </div>
+</div>
+
+<!-- --Nombre - empresa -->
+<div class="modal-container" id="modal-6">
+    <h4 class="modal-pregunta">¿Dónde podemos contactarte?</h4>
+    <div class="modal-links-container">
+        <input class="input-link" type="text" placeholder="+52...">
+        <a id="mod7" href="#" class="following"></a>       
+    </div>
+</div>
+
+<!-- --Correo -->
+<div class="modal-container" id="modal-7">
+    <h4 class="modal-pregunta">¿Cuál es tu correo electrónico?</h4>
+    <div class="modal-links-container">
+        <input class="input-link" type="text" placeholder="+52...">
+        <a id="mod8" href="#" class="following"></a>       
+    </div>
+</div>
+
+<!-- --Agendar llamada -->
+<div class="modal-container" id="modal-8">
+    <h4 class="modal-pregunta">¿Te gustaría hablar con un consultor ahora?</h4>
+    <div class="modal-links-container">
+        <input class="input-link" type="text" placeholder="+52...">
+        <a href="#" class="modal-link">Ir a Whatsapp</a>
+        <a href="#" class="modal-link">Agenda una llamada</a>
     </div>
 </div>
