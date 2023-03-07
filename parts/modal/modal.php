e5e8770a394d739c07dab35944947548ee7c5086
LuiEML <guero560mlb@gmail.com>
2023-03-07 15:54:39 -0600

    nuevos cambios 07-03-2023

--- parts/modal/modal.php
+++ parts/modal/modal.php
@@ -32,11 +32,21 @@
         <img class="persona" src="<?php echo(ICON . 'personas-blanco.svg')?>" alt="personas-blanco">
         <h4 class="modal-pregunta">¿Estás interesado en prestar servicios relacionados con...?</h4>
         <div class="modal-links-container-icons">
-            <a href="#" class="img-modal mod4 leftno"><img class="tele"  src="<?php echo(ICON. 'tele-verde.svg')?>" alt="opcion-image">Televisión</a>
-            <a href="#" class="img-modal mod4 leftno"><img class="radio" src="<?php echo(ICON. 'radio-verde.svg')?>" alt="opcion-image">Radio</a>
-            <a href="#" class="img-modal mod4 leftno"><img class="inter" src="<?php echo(ICON. 'busqueda-verde.svg')?>" alt="opcion-image">Internet</a>
-            <a href="#" class="img-modal mod4 leftno"><img class="fon" src="<?php echo(ICON. 'telefonoVerde.svg')?>" alt="opcion-image">Telefonía</a>
-            <a href="#" class="modal-link mod4 leftno">No estoy seguro</a>
+            <div class="modal-link-img">
+                <a href="#" class="img-modal-tv mod4 leftno"><img class="tele"  src="<?php echo(ICON. 'tele-verde.svg')?>" alt="opcion-image">Televisión</a>
+            </div>
+            <div class="modal-link-img">
+                <a href="#" class="img-modal-radio mod4 leftno"><img class="radio" src="<?php echo(ICON. 'radio-verde.svg')?>" alt="opcion-image">Radio</a>
+            </div>
+            <div class="modal-link-img">
+                <a href="#" class="img-modal-intert mod4 leftno"><img class="inter" src="<?php echo(ICON. 'busqueda-verde.svg')?>" alt="opcion-image">Internet</a>
+            </div>
+            <div class="modal-link-img">
+                <a href="#" class="img-modal-fon mod4 leftno"><img class="fon" src="<?php echo(ICON. 'telefonoVerde.svg')?>" alt="opcion-image">Telefonía</a>
+            </div>
+            <div class="modal-link-img link-no">
+                <a href="#" class="modal-link-no mod4 leftno">No estoy seguro</a>
+            </div>
         </div>
     </div>
 </div>
@@ -46,14 +56,24 @@
     <div class="content-modal-3">
         <span class="btn-regreso left-mod-3">🡰</span>
         <span class="w3-closebtn closed-3">&times;</span>
-        <img class="persona" src="<?php echo(ICON . 'personas-blanco.svg')?>" alt="personas-blanco">
+        <img class="persona-mod-3" src="<?php echo(ICON . 'personas-blanco.svg')?>" alt="personas-blanco">
         <h4 class="modal-pregunta-3">¿Te gustaria recibir asesoría relacionada con algún trámite para...?</h4>
         <div  class="modal-links-container-modal3">
-            <a href="#" class="modal-link-3 mod4 ine left3"><img src="<?php echo(ICON. 'INE-blanco.svg')?>" alt="opcion-image"></a>
-            <a href="#" class="modal-link-3 mod4 segob left3"><img src="<?php echo(ICON. 'segob-blanco.svg')?>" alt="opcion-image"></a>
-            <a href="#" class="modal-link-3 mod4 profeco left3"><img src="<?php echo(ICON. 'profeco-blanco.svg')?>" alt="opcion-image"></a>
-            <a href="#" class="modal-link-3 mod4 ift left3"><img src="<?php echo(ICON. 'ift-blanco.svg')?>" alt="opcion-image"></a>
-            <a href="#" class="modal-link-3 mod4 left3">OTRO</a>
+            <div class="modal-link-padre">
+                <a href="#" class="modal-link-ine mod4 ine left3"><img src="<?php echo(ICON. 'INE-blanco.svg')?>" alt="opcion-image"></a>
+            </div>
+            <div class="modal-link-padre">
+                <a href="#" class="modal-link-segob mod4 segob left3"><img src="<?php echo(ICON. 'segob-blanco.svg')?>" alt="opcion-image"></a>
+            </div>
+            <div class="modal-link-padre">
+                <a href="#" class="modal-link-profeco mod4 profeco left3"><img src="<?php echo(ICON. 'profeco-blanco.svg')?>" alt="opcion-image"></a>
+            </div>
+            <div class="modal-link-padre">
+                <a href="#" class="modal-link-ift mod4 ift left3"><img src="<?php echo(ICON. 'ift-blanco.svg')?>" alt="opcion-image"></a>
+            </div>
+            <div class="modal-link-padre">
+                <a href="#" class="modal-link-3 mod4 left3">OTRO</a>
+            </div>
         </div>
     </div>
 </div>
@@ -117,9 +137,13 @@
         <span class="w3-closebtn closed-8">&times;</span>
         <img class="persona" src="<?php echo(ICON . 'personas-blanco.svg')?>" alt="personas-blanco">
         <h4 class="modal-pregunta">¿Te gustaría hablar con un consultor ahora?</h4>
-        <div class="modal-links-container">
-            <a href="#" class="modal-link">Ir a Whatsapp</a>
-            <a href="#" class="modal-link event-calendar">Agenda una llamada</a>
+        <div class="modal-links-agendar">
+            <div class="modal-agendar">
+                <a target="_blank" href="https://wa.me/5215540370369" class="modal-link">Ir a Whatsapp</a>
+            </div>
+            <div class="modal-agendar">
+                <a href="#" class="modal-link event-calendar">Agenda una llamada</a>
+            </div>
         </div>
     </div>
 </div>
@@ -127,27 +151,27 @@
 
 <!-- --Enviar evento del calendario -->
 <div class="modal-container modal-envent-calendar">
-    <div class="content-modal-event">
+    <div class="content-modal">
         <span class="btn-regreso left-mod-calendar">🡰</span>
         <span class="w3-closebtn closed-event">&times;</span>
         <p  class="event-pregunta">Regístrate en el calendario</p>
         <div class="modal-links-event">
-            <div class="formulario">
                 <div class="form-input">
-                    <label for="inputName" class="col-4 col-form-label">Fecha</label>
+                    <label for="inputName" class="col-form-label">Fecha</label>
                     <input type="date" class="form-control" id="fech1" required />
                 </div>
                 <div class="form-input">
-                    <label for="inputName" class="col-4 col-form-label">Hora inicio</label>
+                    <label for="inputName" class="col-form-label">Hora inicio</label>
                     <input type="time" class="form-control" id="hinicio" required />
                 </div>
                 <div class="form-input">
-                    <label for="inputName" class="col-4 col-form-label">Hora fin</label>
+                    <label for="inputName" class="col-form-label">Hora fin</label>
                     <input type="time" class="form-control" id="hfin" required />
                 </div>
                 <br>
-                <button type="submit" onclick="execute()" class="mod-fin">Enviar</button>
-            </div>
+                <div class="form-input">
+                    <button type="submit" onclick="execute()" class="mod-fin  btn-enviar">Enviar</button>
+                </div>
         </div>
     </div>
 </div>
@@ -155,12 +179,10 @@
 <div class="modal-container modal-final">
     <div class="content-modal-inicio">
         <span class="w3-closebtn closed-fin">&times;</span>    
-        <h4 class="modal-pregunta-inicio">¡Pronto nos pondremos en contacto contigo!</h4>
-        <h6 class="modal-sub-pregunta">¿Quieres hablar con un asesor ahora?</h6>
-        <div class="img-content">
+        <h4 class="modal-pregunta-final">¡Pronto nos pondremos en contacto contigo!</h4>
+        <h6 class="modal-sub-pregunta-fin">¿Quieres hablar con un asesor ahora?</h6>
+        <div class="img-content-final">
             <img  class="person-fin mod2" src="<?php echo(IMG. 'MascotaCentada.webp')?>" alt="opcion-image">
         </div>
     </div>
 </div>
-=======
->>>>>>> main
