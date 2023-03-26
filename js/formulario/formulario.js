1a3ccdd4a921789fa07982d5b197312b34ad7c9b
Ángel Iván Martínez <mgangelivan@gmail.com>
2023-03-25 21:18:57 -0600

    calendario del usuario recibe dias no usados

--- js/formulario/formulario.js
+++ js/formulario/formulario.js
@@ -1,18 +1,16 @@
-let holiDays = [""]; //suppose 17th june is holiday, we can also add others holidays
+let holiDays = localStorage.getItem("fechas");
+holiDays = JSON.parse(holiDays);
 
 function disableHoliday(date) {
-    let string = $.datepicker.formatDate('yy-mm-dd', date);
+    var string = $.datepicker.formatDate('yy-mm-dd', date);
           
-    let filterDate = new Date(string);
-    let day = filterDate.getDay();
-    let isHoliday = ($.inArray(string, holiDays) != -1);
-    return [day != 0 && day !=6 && !isHoliday];
+    var filterDate = new Date(string);
+    var day = filterDate.getDay();
+    var isHoliday = ($.inArray(string, holiDays) != -1);
+    
+    return [day != 0 && day !=6 && !isHoliday]
  }
-
-jQuery( "#date_cliente" ).datepicker({
-    minDate: 0,
-    dateFormat: "yy-mm-dd",
-    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-    dayNamesMin: ['Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vie', 'Sab'],
-    beforeShowDay: disableHoliday,
-});
+        
+ $( "#date_cliente" ).datepicker({
+    beforeShowDay: disableHoliday
+ });
